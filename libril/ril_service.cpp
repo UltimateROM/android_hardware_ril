81759998b56dabd807e3a8cd02214bbe8bee67ed
Shilin Victor <chrono.monochrome@gmail.com>
2017-12-24 21:22:17 +0300

    ril: add more debug
    Change-Id: I26c099bfbf7d355c8aa5f0bb01d8e11f8e2a8228

--- libril/ril_service.cpp
+++ libril/ril_service.cpp
@@ -3947,6 +3947,8 @@ int radio::getNetworkSelectionModeResponse(int slotId,
         populateResponseInfo(responseInfo, serial, responseType, e);
         bool manual = false;
         int serviceClass;
+        RLOGE("getNetworkSelectionModeResponse: responseLen = %d", responseLen);
+
         if (response == NULL || responseLen != sizeof(int)) {
             RLOGE("getNetworkSelectionModeResponse Invalid response: NULL");
             if (e == RIL_E_SUCCESS) responseInfo.error = RadioError::INVALID_RESPONSE;
@@ -5069,6 +5071,8 @@ int radio::getDeviceIdentityResponse(int slotId,
 
         int numStrings = responseLen / sizeof(char *);
         hidl_string emptyString;
+        RLOGE("getDeviceIdentityResponse: numStrings = %d", numStrings);
+
         if (response == NULL || numStrings != 4) {
             RLOGE("getDeviceIdentityResponse Invalid response: NULL");
             if (e == RIL_E_SUCCESS) responseInfo.error = RadioError::INVALID_RESPONSE;
@@ -5400,6 +5404,8 @@ int radio::getImsRegistrationStateResponse(int slotId,
         bool isRegistered = false;
         int ratFamily = 0;
         int numInts = responseLen / sizeof(int);
+        RLOGE("getImsRegistrationStateResponse: numInts = %d", numInts);
+
         if (response == NULL || numInts != 2) {
             RLOGE("getImsRegistrationStateResponse Invalid response: NULL");
             if (e == RIL_E_SUCCESS) responseInfo.error = RadioError::INVALID_RESPONSE;
